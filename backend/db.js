f094a51d795487c2b3d71c7621d327fbbe75262a
Aditya-Kumar25 <adityaashu.dynmo@gmail.com>
2025-11-30 23:49:41 +0530

    Start of the project

---
+++ backend/db.js
@@ -0,0 +1,14 @@
+const mongoose  = require("mongoose")
+mongoose.connect()
+const userSchema = new mongoose.Schema({
+    firstName :String,
+    lastName: String,
+    username: String,
+    password:String
+
+})
+const User = mongoose.model("User",userSchema)
+
+module.exports = {
+    User
+}
