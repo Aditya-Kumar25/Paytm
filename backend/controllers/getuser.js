daac57cbc1636a6ab0bbb2c0b49a062d3abafc52
Aditya-Kumar25 <adityaashu.dynmo@gmail.com>
2026-05-26 19:48:45 +0530

    fixed fetching users in backend

--- backend/controllers/getuser.js
+++ backend/controllers/getuser.js
@@ -1,27 +1,34 @@
-const express = require("express")
-const {User} = require("../db")
+const express = require("express");
+const { User } = require("../db");
 
-async function GetUsers(req,res) {
-    const filter = req.query.filter||"";
+async function GetUsers(req, res) {
+  const filter = req.query.filter || "";
 
-    const user = await User.find(
-        $or[{
-            firstname:{
-                $regex:filter
-            },
-            lastname:{
-                $regex:filter
-            }
-        }]
-    )
+  const user = await User.find({
+    $or: [
+      {
+        firstname: {
+          $regex: filter,
+        },
+      },
+      {
+        lastname: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
 
-    res.json({
-        user:users.map(user=>{
-            username:user.username;
-            firstname:user.firstname;
-            lastname:user.lastname;
-            _id:user._id
-
-        })
-    })
+  res.json({
+    user: users.map((user) => {
+      return {
+        username: user.username,
+        firstname: user.firstname,
+        lastname: user.lastname,
+        _id: user._id,
+      };
+    }),
+  });
 }
+
+module.exports = GetUsers;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const SignIn = require("../controllers/signin")
 const SignUp = require("../controllers/signup")
 const Update = require("../controllers/pchange")
+const GetUsers = require("../controllers/getuser")
 const {authMiddleware} = require("../middleware")
 
 
@@ -10,6 +11,7 @@ const  router = express.Router();
 
 router.post('/signin',SignIn);
 router.post('/signup',SignUp);
+router.get('/getuser',GetUsers)
 router.patch('/update',authMiddleware,Update)
 
 module.exports = router;
