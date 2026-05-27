4ac9f0d4bea9f945223b9f9f3ea441755a53ec77
Aditya-Kumar25 <adityaashu.dynmo@gmail.com>
2026-05-27 06:08:32 +0530

    search fixes

--- backend/controllers/getuser.js
+++ backend/controllers/getuser.js
@@ -1,34 +1,35 @@
-const express = require("express");
 const { User } = require("../db");
 
 async function GetUsers(req, res) {
   const filter = req.query.filter || "";
 
-  const user = await User.find({
+  const users = await User.find({
     $or: [
       {
         firstname: {
           $regex: filter,
+          $options:"i"
         },
       },
       {
         lastname: {
           $regex: filter,
+          $options:"i"
         },
       },
     ],
   });
 
   res.json({
-    user: users.map((user) => {
-      return {
-        username: user.username,
-        firstname: user.firstname,
-        lastname: user.lastname,
-        _id: user._id,
-      };
-    }),
+    user: users.map((user) => ({
+      username: user.username,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      _id: user._id,
+    })),
   });
+  console.log(users)
+  console.log(filter)
 }
 
 module.exports = GetUsers;
